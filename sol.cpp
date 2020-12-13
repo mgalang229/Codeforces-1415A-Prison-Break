0eb8f98f06ec5b7de9ded7958fc96ec73cd00122
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-13 20:08:53 +0800

    Create sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ar array
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+
+void solve() {
+	int n, m, x, y;
+	cin >> n >> m >> x >> y;
+	cout << max(x-1, n-x)+max(y-1, m-y) << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t;
+	cin >> t;
+	while(t--)
+		solve();
+}
